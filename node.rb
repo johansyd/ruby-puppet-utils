a4307f1972f54c8a5b2fe38d1abf87cfd6dd83a9
Johan Sydseter <johan.sydseter@startsiden.no>
2012-12-31 19:11:34 +0100

    Added the node manifest

---
+++ node.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/ruby
+# A simple script which fetches external nodes from Foreman
+# you can basically use anything that knows how to get http data, e.g. wget/curl
+# etc.
+# author: Johan Sydseter, <johan.sydseter@startsiden.no>
+
+# Foreman destination
+# Define FOREMAN_HOST, FOREMAN_PORT and FOREMAN_METHOD in the system 
+# environment to use foreman. Falls back to the node definitions in 
+# ~/manifests/yaml
+
+foreman_host = ENV['FOREMAN_HOST']     || 'foreman.startsiden.no'
+foreman_port = ENV['FOREMAN_PORT']     || '80'
+foreman_method = ENV['FOREMAN_METHOD'] || 'http'
+
+foreman_url = foreman_method + '://' + foreman_host + ':' + foreman_port
+
+node = ARGV[0]
+foreman_url += "/node/#{node}?format=yml" 
+
+date = `date +"%b %d %T" | tr -d "\n"`
+script = $0
+
+
+require 'net/http'
+require 'net/https' if foreman_url =~ /^https/
+
+url = URI.parse(foreman_url)
+con = Net::HTTP.new(url.host, url.port)
+
+# decide whether to use ssl
+if foreman_url =~ /^https/
+    con.use_ssl = (url.scheme == 'https')
+end
+
+# start the call to forman
+begin
+
+    req = Net::HTTP::Get.new(foreman_url)
+    res = con.start { |http| http.request(req) }
+
+rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, SocketError,
+       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+    $stderr.puts date + ' ' + script + ": ERROR reaching forman: " + e
+end
+
+case res
+
+# Return the result if the forman call was successfull
+when Net::HTTPOK
+    puts res.body
+else
+    # Return the yaml content of the file in yaml/$node_name
+    # cat_result = `cat yaml/#{ARGV[0]}`
+    $stderr.puts date + ' ' + script + ": ERROR retrieving node %s from Forman." % [node]
+    $stderr.puts date + ' ' + script + 
+        ": TRACE retrieving node %s from yaml/%s.yml" % [node, node]
+    cat_result = `cat yaml/#{ARGV[0]}.yml 2> /dev/null || echo 0 | tr -d "\n"`
+
+    if cat_result != '0'
+        puts cat_result
+    else
+        # There where no such file return a empty yaml definition
+        $stderr.puts date + ' ' + script + 
+            ": ERROR retrieving node %s from yaml/%s.yml" % [node, node]
+        $stderr.puts date + ' ' + script +
+            ": TRACE fall back to empty node definition."
+        puts "\n---\nclasses:"
+    end
+end

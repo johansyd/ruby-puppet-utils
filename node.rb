550ff054ce0082ea1b981873e550a1ddebe730a1
Johan Sydseter <johan.sydseter@startsiden.no>
2013-01-03 17:10:30 +0100

    Added global variable and changed the yaml directory location to make it more extendible

--- node.rb
+++ node.rb
@@ -7,12 +7,12 @@
 # Foreman destination
 # Define FOREMAN_HOST, FOREMAN_PORT and FOREMAN_METHOD in the system 
 # environment to use foreman. Falls back to the node definitions in 
-# ~/manifests/yaml
+# ../manifests/yaml
 
 foreman_host = ENV['FOREMAN_HOST']     || 'foreman.startsiden.no'
 foreman_port = ENV['FOREMAN_PORT']     || '80'
 foreman_method = ENV['FOREMAN_METHOD'] || 'http'
-
+yaml_node_dir = ENV['YAML_NODE_DIR'] || '../manifests/yaml'
 foreman_url = foreman_method + '://' + foreman_host + ':' + foreman_port
 
 node = ARGV[0]
@@ -52,17 +52,27 @@ when Net::HTTPOK
 else
     # Return the yaml content of the file in yaml/$node_name
     # cat_result = `cat yaml/#{ARGV[0]}`
+    if Dir[yaml_node_dir].nil?
+        $stderr.puts date + ' ' + script + "Could not find #{yaml_node_dir}";
+        $stderr.puts date + ' ' + script + 
+            "When no forman server is present, the default is to search for \
+            a yaml file with the same name as the puppet agent host for \
+            instantiation of puppet modules. When there are no directory where \
+            these yaml files exist. node.rb fails."
+        puts "\n---\nclasses:"
+        exit
+    fi
     $stderr.puts date + ' ' + script + ": ERROR retrieving node %s from Forman." % [node]
     $stderr.puts date + ' ' + script + 
-        ": TRACE retrieving node %s from yaml/%s.yml" % [node, node]
-    cat_result = `cat yaml/#{ARGV[0]}.yml 2> /dev/null || echo 0 | tr -d "\n"`
+        ": TRACE retrieving node %s from #{yaml_node_dir}/%s.yml" % [node, node]
+    cat_result = `cat #{yaml_node_dir}/#{ARGV[0]}.yml 2> /dev/null || echo 0 | tr -d "\n"`
 
     if cat_result != '0'
         puts cat_result
     else
         # There where no such file return a empty yaml definition
         $stderr.puts date + ' ' + script + 
-            ": ERROR retrieving node %s from yaml/%s.yml" % [node, node]
+            ": ERROR retrieving node %s from #{yaml_node_dir}/%s.yml" % [node, node]
         $stderr.puts date + ' ' + script +
             ": TRACE fall back to empty node definition."
         puts "\n---\nclasses:"
